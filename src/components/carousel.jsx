778162676e81b0c83b07a76b1eb54e9fa3110c05
Tolemy Kashyap <kashyaptolemy@gmail.com>
2023-03-22 06:28:11 +0530

    hover done on carousel

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -8,8 +8,16 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import 'swiper/swiper-bundle.min.css'
+import { useState } from "react";
 
 export default function Carousel({slides}){
+  const[hovered,sethovered] =useState(false);
+  const handleMouseEnter =()=>{
+    sethovered(true);
+  }
+  const handleMouseLeave =()=>{
+    sethovered(true);
+  }
     return(
         <div className="carouselcontainer">
             <Swiper
@@ -24,7 +32,7 @@ export default function Carousel({slides}){
             >
               {slides.map((slide)=>(
                 <SwiperSlide key={slide.id}>
-                  <div className="sliderbox">
+                  <div className="sliderbox" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                     <div className="withouthover">
                       <img className ="sliderimg" src={slide.background_image} alt ={slide.name}></img>
                       <h4 className="titleslider">{slide.name}</h4>
@@ -40,9 +48,9 @@ export default function Carousel({slides}){
                       }
                       <h4 className="ratingslider">&#11088;<span className="ratingslider"> ({slide.rating}/{slide.rating_top})</span></h4>
                     </div>
-                    {/* <div className="withover">
+                    <div className="withover">
                         <h2 className="hovertitle">{slide.name}</h2>
-                    </div> */}
+                    </div>
                   </div>
                   
                 </SwiperSlide>
