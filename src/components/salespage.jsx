156dde5c25d4b36169bf60780943c34b8f551ac3
Tolemy Kashyap <kashyaptolemy@gmail.com>
2023-03-25 23:30:40 +0530

    infopage partailly created

--- src/components/salespage.jsx
+++ src/components/salespage.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import { useState } from "react";
 
 
-export default function Salespage(){
-    const [saleTime,setSaleTime] =useState("");
-    const time =new Date().toLocaleString();
-    // setSaleTime(time);
-    return(
+export default function Salespage() {
+    return (
         <div className="page1">
             <h2 className="salesbox">MARCH MADNESS SALE!!<br />Mar 25-Mar 29</h2>
         </div>
--- src/pages/Infopage.jsx
+++ src/pages/Infopage.jsx
@@ -1,13 +1,26 @@
 import { useParams } from "react-router-dom"
 import data from "../mock.json"
 import "../App.css"
-export default function Infopage({slides}){
-    // console.log(name);
-    const params = useParams();
-    const info = data[params.id]
+export default function Infopage() {
+  const params = useParams();
+  const info = data[params.id]
+  console.log(info.platforms)
   return (
-    <div className="text">
-      {info.name}
+    <div className="infopage">
+      <div className="infopageContainer">
+        <div className="infotitle"><span className="infopageTitle">{info.name}</span></div>
+        <div className="infopara">
+
+          <h4>Minimum Requirements</h4>
+          {info.platforms.map((item) => (
+            <p>
+              Was released in {item.platform.name} on {item.released_at}
+              <br />
+              Minimum Requirements: {`${item.requirements_en?.minimum || "none"}`}
+            </p>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
