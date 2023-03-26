324e9cea475ed0a9ff97e42333ecb40d1ebccd60
Tolemy Kashyap <kashyaptolemy@gmail.com>
2023-03-26 16:22:51 +0530

    bg changed

--- src/components/salespage.jsx
+++ src/components/salespage.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Bg from "../images/bgdb2.png"
+import Bg from "../images/bgdb4.png"
 
 export default function Salespage() {
     return (
         <img src={Bg} alt="background" className="page1">
-            {/* <h2 className="salesbox">MARCH MADNESS SALE!!<br />Mar 25-Mar 29</h2> */}
         </img>
     )
 } 
