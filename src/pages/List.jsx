6e41308565b33ff1982c2591a2090aefdf57aaa9
Tolemy Kashyap <kashyaptolemy@gmail.com>
2023-03-28 00:06:46 +0530

    router for category created

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import { useParams } from "react-router-dom"
-import data from "../mock.json"
+import slides from "../mock.json"
 import "../App.css"
-const List = ({}) => {
-  const { slides } = useParams();
-
-  const slideObjects = slides
-    ? slides.split(',').map((slide, index) => {
-        if (slide.includes(':')) {
-          const [url, title] = slide.split(':');
-          return { id: index, url, title };
-        } else {
-          return { id: index, url: slide, title: '' };
-        }
-      })
-    : [];
+const List = ({ }) => {
+  const { id } = useParams();
+  const items = slides.filter(slide => (
+    slide.tags.some(tag => tag.name = { id })
+  ))
   return (
     <div className='listContainer'>
-      ebvrissfs
+      <div className='listBox'>
+        {/* {items.map.slice(0,2)((item) => (
+          <div className='listItem2'>
+            <img src={item.background_image} alt={item.name} className="listImage2"></img>
+            <div>{item.name}</div>
+          </div>
+        ))}
+        {items.map.slice(2,5)((item) => (
+          <div className='listItem3'>
+            <img src={item.background_image} alt={item.name} className="listImage3"></img>
+            <div>{item.name}</div>
+          </div>
+        ))} */}
+      </div>
     </div>
   )
 }
